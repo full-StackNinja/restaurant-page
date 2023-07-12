db8624fd5879ec59ac98b68e01e8f4eb1d55c349
Imran Hussain <imranhussain399@gmail.com>
2023-07-12 09:07:36 +0000

    initial setup of the project directories

---
+++ bundle.js
@@ -0,0 +1,10 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+iframe.sandbox = 'allow-same-origin allow-scripts allow-popups allow-forms';
+console.log('Hello there am Okk')
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7O0FBQUE7QUFDQSxpQyIsInNvdXJjZXMiOlsid2VicGFjazovL3Jlc3RhdXJhbnQtcGFnZS8uL3NyYy9pbmRleC5qcyJdLCJzb3VyY2VzQ29udGVudCI6WyJpZnJhbWUuc2FuZGJveCA9ICdhbGxvdy1zYW1lLW9yaWdpbiBhbGxvdy1zY3JpcHRzIGFsbG93LXBvcHVwcyBhbGxvdy1mb3Jtcyc7XG5jb25zb2xlLmxvZygnSGVsbG8gdGhlcmUgYW0gT2trJykiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
